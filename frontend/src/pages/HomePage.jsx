bee1663efbac04ff6fd0be724293a0d8c3bacefe
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-04-10 01:59:09 +0200

    Set up product fetching logic and load products on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
-import { Container, Text, VStack } from '@chakra-ui/react';
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useProductStore } from '../store/product';
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log("products", products);
+
   return (
     <Container maxW={"container.xl"} py={12}>
       <VStack spacing={8}>
@@ -14,6 +24,18 @@ const HomePage = () => {
         >
           Current Products 🚀
         </Text>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {/* Product cards will go here */}
+        </SimpleGrid>
 
         <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
           No products found 😢{" "}
